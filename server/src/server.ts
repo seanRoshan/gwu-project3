743a5fc6fb6494374f41c6b1bd21df83911bc92d
Sean Roshan <sean.roshan.91@gmail.com>
2025-04-17 18:43:47 -0400

    fixed the path

--- server/src/server.ts
+++ server/src/server.ts
@@ -34,10 +34,12 @@ const startApolloServer = async () => {
   );
 
   if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/dist")));
+    const clientBuildPath = path.join(__dirname, "..", "client", "dist");
 
-    app.get("*", (_req: Request, res: Response) => {
-      res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+    app.use(express.static(clientBuildPath));
+
+    app.get("*", (_req, res) => {
+      res.sendFile(path.join(clientBuildPath, "index.html"));
     });
   }
 
